2237b4c2c0c04bb55f083ba109414651bf45fe3b
Anna <ann111.ruu@gmail.com>
2023-03-12 03:27:17 +0300

    09.03

--- arcivator.c
+++ arcivator.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <string.h>
+#include <sys/stat.h>
 
 
 #define SIZE  512
@@ -9,57 +10,53 @@ int main()
 {
     char afile[50];
     char directory[50];
-    FILE *file;
-   // char buffer[100];
-
-    size_t num;
-   // char   buffer[10]; //буффер для записи
-
-    char   buffer[SIZE]; //буффер для записи
+    char buffer[SIZE];
+    FILE *file, *archive;
+    size_t symbols;
+    DIR *mydir;
+    struct dirent *in;
 
     printf ("Введите путь к архиву: ");
     scanf("%s", afile);
-    FILE *archive;  
 
     printf ("Введите путь к директории для архивации: \n");
     scanf("%s", directory);
 
-    //DIR *mydir = opendir(directory);
-
-    //struct dirent *entry;
-   // while(entry = readdir(mydir)) 
-
-    DIR *mydir;
-    struct dirent *entry;
     mydir = opendir(directory);
- /*     if (!mydir) {
-        perror("diropen");
-        exit(1);
-    };
-  */
-  //  archive = fopen (afile, "wb");
-    while ( (entry = readdir(mydir)) != NULL)
+      if (!mydir) 
+      {
+        printf("Не удалось открыть директорию.\n");
+        return 3;
+      };
+
+    while ( (in = readdir(mydir)) != NULL)
   {
-    printf("%s", entry->d_name);
-    if (strcmp(".", entry->d_name) == 0 || strcmp("..", entry->d_name) == 0) continue;
-    if((file = fopen(entry->d_name, "r")) == NULL)   //обработка ошибки открытия файлов
+    char path[SIZE];
+    snprintf(path, sizeof path, "%s/%s", directory, in->d_name);
+
+    if (strcmp(".", in->d_name) == 0 || strcmp("..", in->d_name) == 0) continue;
+
+    if((file = fopen(path, "r")) == NULL)
     {
-        printf("Could not open file.\n");
+        printf("Не удалось открыть файл.\n");
         return 1;
     }
  
     if((archive = fopen(afile, "a+")) == NULL)
     {
-        printf("Could not open afile.\n");
+        printf("Не удалось открыть архив.\n");
         return 2;
         fclose(archive);
     }
 
-    archive = fopen(afile, "a+");  
-    while((num = fread(buffer, sizeof(char), SIZE, file)) > 0)  //чтение копируемого файла до конца
+    file = fopen(path, "r");
+    archive = fopen(afile, "a+"); 
+
+    while((symbols = fread(buffer, sizeof(char), SIZE, file)) > 0)
     {
-      fwrite(buffer, sizeof(char), num, archive);//запись копии в новый файл
+      fwrite(buffer, sizeof(char), symbols, archive);
     }
+
     fclose(archive);
     fclose(file);
   }
